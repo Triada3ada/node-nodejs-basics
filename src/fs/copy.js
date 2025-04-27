911762ed920f310378818237e0962cb9461b5330
Adis <adiskawork@gmail.com>
2025-04-28 04:08:54 +0600

    added copy.js function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,25 @@
+import * as fs from 'node:fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
 const copy = async () => {
     // Write your code here 
+    try {
+        const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+        const filesDir = await fs.promises.opendir(`${__dirname}` + '/files');
+
+        if (filesDir instanceof fs.Dir && !fs.existsSync(`${__dirname}` + '/files_copy')) { 
+            fs.cp(`${__dirname}` + '/files', `${__dirname}` + '/files_copy', {recursive: true}, (err) => {
+                console.log('files copied successfully');
+            })
+        } else {
+            throw new Error("either files dir does not exist or files_copy already created");
+        }
+    } catch (error) {
+        throw new Error(error)
+    }
+
 };
 
 await copy();
