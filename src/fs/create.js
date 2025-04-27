9eff82c09f83bb49581324cfa2931f6ae5e9b5c8
Adis <adiskawork@gmail.com>
2025-04-27 22:47:02 +0600

    added fs/create.js solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,26 @@
+import * as fs from 'node:fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
 const create = async () => {
-    // Write your code here 
+    // Write your code here
+    try {
+        const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+        fs.readFile(`${__dirname}` + '/files/fresh.txt', (err, data) => {
+            if (!data) {
+                const writeStream = fs.createWriteStream(`${__dirname}` + '/files/fresh.txt', {
+                    encoding: 'utf-8',
+                });
+                writeStream.write('I am fresh and young');
+                writeStream.end();
+            } else {
+                 throw new Error('file already exists')
+            }
+        });
+    } catch (error) {
+        throw new Error(error);
+    }
 };
 
 await create();
