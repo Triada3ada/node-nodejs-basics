bf1474ee2ed3e6c777d6c9b9be544463e2415572
Adis <adiskawork@gmail.com>
2025-04-29 00:50:42 +0600

    added env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,20 @@
 const parseEnv = () => {
-    // Write your code here 
+    // Write your code here
+    const startsWithRSS = /^RSS_/;
+
+    process.env.RSS_school = 'cool';
+    process.env.RSS_Me = 'very cool';
+
+
+    const onlyRSSvariables = Object.keys(process.env)
+        .map((value, i) => {
+            return value.match(startsWithRSS);
+        })
+        .filter((item) => item);
+        
+    onlyRSSvariables.forEach(element => {
+        console.log(`${element['input']}=`+ process['env'][element['input']]);
+    });    
 };
 
 parseEnv();
