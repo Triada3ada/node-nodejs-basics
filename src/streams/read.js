40649979efafd3ffb560071ddf038ae18b354ee7
Adis <adiskawork@gmail.com>
2025-04-29 21:52:54 +0600

    added streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
 const read = async () => {
     // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const fileToRead = fs.createReadStream(`${__dirname}` + '/files/fileToRead.txt',)
+    fileToRead.on('data', (chunk) =>{
+        process.stdout.write(chunk.toString());
+    })
 };
 
 await read();
