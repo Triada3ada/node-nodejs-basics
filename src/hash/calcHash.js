f856b3dcb2193a932d314aeba572008d0bcc93bf
Adis <adiskawork@gmail.com>
2025-04-29 19:39:34 +0600

    added calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import { createHash } from 'node:crypto';
+import * as fs from 'fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
 const calculateHash = async () => {
-    // Write your code here 
+    // Write your code here
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+    const filetoRead = await fs.promises.readFile(
+        `${__dirname}` + '/files/fileToCalculateHashFor.txt',
+        {
+            encoding: 'utf-8',
+        }
+    );
+    process.stdout.write(createHash('sha256').update(filetoRead).digest('hex'));
 };
 
 await calculateHash();
