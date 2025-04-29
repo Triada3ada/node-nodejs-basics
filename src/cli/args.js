42b01635f56f4731d21cabc727d6629445f120d6
Adis <adiskawork@gmail.com>
2025-04-29 18:28:21 +0600

    added args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,21 @@
+import * as util from 'node:util'
+
 const parseArgs = () => {
     // Write your code here 
+    const options = {
+        propName: {
+          type: 'string',
+        },
+        prop2Name: {
+          type: 'string'
+        }
+      };
+
+    const {values, positionals} = util.parseArgs({args: process.argv, options: options, allowPositionals: true});
+    
+    for (const key in values) {
+        console.log(`${key} is ` + values[key]);
+    }
 };
 
 parseArgs();
