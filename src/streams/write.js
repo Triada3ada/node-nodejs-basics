11c23fe812cf48e68b78294d87780739cc62f2e7
Adis <adiskawork@gmail.com>
2025-04-29 22:13:39 +0600

    added streams/write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
 const write = async () => {
-    // Write your code here 
+    // Write your code here
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+    const fileToWrite = fs.createWriteStream(`${__dirname}` + '/files/fileToWrite.txt');
+
+    process.stdin.on('data', (data) => {
+        fileToWrite.write(data);
+        process.exit();
+    });
 };
 
 await write();
