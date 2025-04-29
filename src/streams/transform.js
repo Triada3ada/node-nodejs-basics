83d8c29199f64564820d04d7e4a9a83997089b60
Adis <adiskawork@gmail.com>
2025-04-30 04:36:58 +0600

    added streams/transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,43 @@
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+import { compose, Transform } from 'node:stream';
+import { finished } from 'node:stream/promises';
+
 const transform = async () => {
-    // Write your code here 
+    // Write your code here
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+    process.stdin.on('data', async (data) => {
+        const firstGen = compose(async function* () {
+            yield data;
+        }());
+
+        const reverseStr = compose(async function*(source) {
+            for await (const chunk of source) {
+                const reverse = (value) => {
+                    return Array.from(String(value || ''))
+                        .reverse()
+                        .join('');
+                };
+
+                yield reverse(chunk);
+            }
+        });
+
+        let accum = '';
+
+        const combinator = compose(async function(source) {
+            for await (const chunk of source) {
+                accum += chunk;
+            }
+          })
+
+        await finished(compose(firstGen, reverseStr, combinator));
+
+        console.log(accum);
+
+        process.exit()
+    });
 };
 
 await transform();
