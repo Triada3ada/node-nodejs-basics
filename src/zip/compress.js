f916b053393793ae3d611ba2dffb1aa93a8456f4
Adis <adiskawork@gmail.com>
2025-04-30 21:05:17 +0600

    added zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,15 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+import * as zlib from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+    // Write your code here
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const fileToRead = fs
+        .createReadStream(`${__dirname}` + '/files/fileToCompress.txt')
+        .pipe(zlib.createGzip())
+        .pipe(fs.createWriteStream(`${__dirname}` + '/files/archive.gz'));
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,15 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+import * as zlib from 'zlib';
+
 const decompress = async () => {
     // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const fileToRead = fs
+        .createReadStream(`${__dirname}` + '/files/archive.gz')
+        .pipe(zlib.createGunzip())
+        .pipe(fs.createWriteStream(`${__dirname}` + '/files/fileToCompress.txt'));
 };
 
 await decompress();
